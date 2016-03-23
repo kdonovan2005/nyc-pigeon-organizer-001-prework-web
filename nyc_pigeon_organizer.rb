ae57de75fe9b467315d21783680ad376905e4623
kdonovan2005 <kdonovan2005@gmail.com>
2016-03-23 09:56:25 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = {}
+
+  data.each do |att, valueHash|
+    valueHash.each do |trait, nameArray|
+      nameArray.each do |name|
+        if pigeons.include?(name) && pigeons[name].include?(att)
+          pigeons[name][att] << trait.to_s
+        elsif pigeons.include?(name) && (!(pigeons[name].include?(att)))
+          pigeons[name].merge!(att => [trait.to_s])
+        else
+          pigeons[name] = {att => [trait.to_s]}
+        end
+      end
+    end
+  end
+  pigeons
 end
